5f56a9dedc9e3dc1a70933a6c278a0905a5fadaf
allen.w <koomoxs@gmail.com>
2022-02-06 21:25:35 +0800

    commit

--- sqlparser/export.go
+++ sqlparser/export.go
@@ -2,9 +2,22 @@ package sqlparser
 
 import (
 	"os"
+    "path/filepath"
 )
 
+func MkdirAll(p string) (err error) {
+    if _, err = os.Stat(p); os.IsNotExist(err){
+        if err = os.MkdirAll(p, os.ModePerm); err != nil {
+            return
+        }
+    }
+    return
+}
+
 func ExportFile(filename, tagField string, data []MetadataTable) error {
+    if err := MkdirAll(filepath.Dir(filename)); err != nil {
+        return err
+    }
 	f, err := os.OpenFile(filename, os.O_RDWR | os.O_CREATE | os.O_TRUNC, os.ModePerm)
     if err != nil {
     	return err
@@ -22,6 +35,9 @@ func ExportFile(filename, tagField string, data []MetadataTable) error {
 }
 
 func WriteFile(b string, filename string) error {
+    if err := MkdirAll(filepath.Dir(filename)); err != nil {
+        return err
+    }
     f, err := os.OpenFile(filename, os.O_RDWR | os.O_CREATE | os.O_TRUNC, os.ModePerm)
     if err != nil {
         return err
